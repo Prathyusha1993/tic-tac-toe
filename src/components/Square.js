005b0051d73b849ec512bf7099dae70024df2e36
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-08-19 17:55:02 -0700

    tic tac toe initialization, styling and patters component

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+const Square = ({val, chooseSquare}) => {
+  return (
+    <div onClick={chooseSquare} className='square'>{val}</div>
+  )
+}
+
+export default Square;
